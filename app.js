43356ce7512c47c451616caded24fcb437eab69a
Aleksander <aleksanderroztrop@gmail.com>
2019-06-16 22:19:53 +0200

    uploading file code

--- app.js
+++ app.js
@@ -1,31 +1,39 @@
 const BASEURL = "http://localhost:8080/";
+let username = document.getElementById("login").value;
+let password = document.getElementById("password").value;
+action("olek", "password");
+function action(username, password) {
+    getUsersAW();
+    async function getUsersAW() {
+        try {
+            const result = await
+                fetch(`${BASEURL}login`, {
+                    method: "post",
+                    body: JSON.stringify({"username": "olek",
+                    "password": "password"}),
+                    headers: {
+                        "Content-type": "application/json"
+                        // "Authorization": "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJvbGVrIiwiZXhwIjoxNTYxNTMzMzU0fQ.-4E9yeBww5b_XXIABEaEYXTrDn7k7Q63T7PBDHt1WVxl6ZD-6iBFj1sHPvdr-yWQw89bte95l6bIYyGnEqntKw"
+                    }
+                });
+            const data = await result.json();
+            // console.log(data);
+            // console.log(data[0].nickname);
+            // for (let i = 0; i < data.length; i++) {
+            //     let newDiv = document.createElement("li");
+            //     newDiv.innerHTML=`<div class ='user'>${data[i].nickname}</div>`;
+            //     // newDiv.appendChild(ne);
+            //     document.getElementById("usersList").appendChild(newDiv);
+            // }
 
-async function getUsersAW() {
-    try {
-        const result = await
-            fetch(`${BASEURL}user`,{
-                method: "get",
-                headers: {
-                    "Content-type": "application/json; charset=UTF-8",
-                    "Authorization": "Basic b2xlazpvbGFrcw=="
-                }
-            });
-        const data = await result.json();
-        console.log(data);
-        console.log(data[0].nickname);
-        for (let i = 0; i < data.length; i++) {
-            let newDiv = document.createElement("li");
-            newDiv.innerHTML=`<div class ='user'>${data[i].nickname}</div>`;
-            // newDiv.appendChild(ne);
-            document.getElementById("usersList").appendChild(newDiv);
+            return data;
+        } catch (error) {
+            console.log(error + '  hehe');
+
         }
-
-        return data;
-    } catch (error) {
-        console.log(error + '  hehe');
-
-    }
+    };
 };
 
-getUsersAW();
+
+
 
--- components/heroes.js
+++ components/heroes.js
@@ -1,6 +1,15 @@
 import { HeroesDetails, Olek} from "./heroesDetails.js";
 import {HeroesBiography} from "./heroesBiography.js";
 
+let date =new Date(1560631188000);
+console.log(date.getTimezoneOffset());
 
+function createDateAsUTC(date) {
+    return new Date(Date.UTC(date.getFullYear(), date.getMonth(), date.getDate(), date.getHours(), date.getMinutes(), date.getSeconds()));
+}
+
+function convertDateToUTC(date) { 
+    return new Date(date.getUTCFullYear(), date.getUTCMonth(), date.getUTCDate(), date.getUTCHours(), date.getUTCMinutes(), date.getUTCSeconds()); 
+}
 
 
