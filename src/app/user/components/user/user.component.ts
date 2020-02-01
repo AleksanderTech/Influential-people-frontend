5ef0447d6158f292893874761a142f0e94e043b1
Aleksander Roztropiński <aleksander.roztropinski@cumulocity.com>
2020-02-01 03:10:42 +0100

    Testing file upload

--- src/app/user/components/user/user.component.ts
+++ src/app/user/components/user/user.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from 'src/app/core/services/authentication.service';
+import { HttpHeaders, HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-user',
@@ -8,9 +9,30 @@ import { AuthenticationService } from 'src/app/core/services/authentication.serv
 })
 export class UserComponent implements OnInit {
 
-  constructor(private authService: AuthenticationService) { }
+  img: File;
+
+  constructor(private authService: AuthenticationService, private httpClient: HttpClient) { }
 
   ngOnInit() {
+  }
+
+  uploadFile(event: Event) {
+    console.log(event.target['files']);
+    let file = event.target['files'][0];
+    if (file.length === 0)
+      return;
+    let mimeType = file.type;
+    if (mimeType.match(/image\/*/) == null) {
+      return;
+    }
+    let reader = new FileReader();
+    this.img = file;
+    reader.readAsDataURL(file);
+    reader.onload = event => {
+      const formData = new FormData();
+      formData.append('image', this.img);
+      this.httpClient.put('http://localhost:8080/hero/Aristotle1/image', formData).subscribe();
+    }
   }
 
   logout() {
